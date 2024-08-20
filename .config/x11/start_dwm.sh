a5d4bd4d8020384ad856c8dbffef1e2a1aface53
KSHMR1337 <marko.zare1234@hotmail.com>
2024-08-20 14:22:09 +0200

    Restart main branch

--- .config/x11/start_dwm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-paperview --compositor /home/zare/.local/share/background/bladerunner 20 0 0 1920 1080 /home/zare/.local/share/background/synthwavecity 10 1920 0 2560 1440 /home/zare/.local/share/background/rainycity 20 4480 0 1920 1080&
-mpd &
-picom --backend glx &
-dwmblocks & 
-exec dwm
-
-
