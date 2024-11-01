3737af634b2392a03af76a09da30107c60327ef1
KSHMR1337 <marko.zare1234@hotmail.com>
2024-11-01 13:16:50 +0100

    Added yazi config

---
+++ .config/yazi/init.lua
@@ -0,0 +1,30 @@
+-- Setup full boarders
+
+require("full-border"):setup({
+	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
+	type = ui.Border.ROUNDED,
+})
+
+-- Symlinks in status bar
+
+function Status:name()
+	local h = self._tab.current.hovered
+	if not h then
+		return ui.Line({})
+	end
+	local linked = ""
+	if h.link_to ~= nil then
+		linked = " -> " .. tostring(h.link_to)
+	end
+	return ui.Line(" " .. h.name .. linked)
+end
+
+-- Show user and host in header
+
+Header:children_add(function()
+	if ya.target_family() ~= "unix" then
+		return ui.Line({})
+	end
+	-- return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("#DC143C")
+	return ui.Span("K5HMЯ" .. "@" .. ya.host_name() .. ":"):fg("#DC143C")
+end, 500, Header.LEFT)
