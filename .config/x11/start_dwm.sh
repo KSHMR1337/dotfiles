2bef933be75294789acb91f033735791e8dba436
KSHMR1337 <marko.zare1234@hotmail.com>
2024-11-18 23:07:57 +0100

    Added automatic monitor configurations.

--- .config/x11/start_dwm.sh
+++ .config/x11/start_dwm.sh
@@ -1,9 +1,28 @@
 #!/bin/zsh
 
-paperview --compositor /home/zare/.local/share/background/bladerunner 20 0 0 1920 1080 /home/zare/.local/share/background/synthwavecity 10 1920 0 2560 1440 /home/zare/.local/share/background/rainycity 20 4480 0 1920 1080&
-mpd &
-picom --backend glx &
-dwmblocks & 
-exec dwm
+monitor_count=$(xrandr | grep -c " connected ")
 
+case "$monitor_count" in
+        2)
+                paperview --compositor /home/zare/.local/share/background/bladerunner 5 0 0 1920 1080 &
+                ;;
+
+        3)
+                paperview --compositor /home/zare/.local/share/background/bladerunner 20 0 0 1920 1080 /home/zare/.local/share/background/synthwavecity 10 1920 0 1920 1080 /home/zare/.local/share/background/rainycity 20 4480 0 1920 1080 &
+                ;;
+
+        4)
+                paperview --compositor /home/zare/.local/share/background/bladerunner 20 0 0 1920 1080 /home/zare/.local/share/background/synthwavecity 10 1920 0 2560 1440 /home/zare/.local/share/background/rainycity 20 4480 0 1920 1080 &
+                ;;
+
+        *)
+                paperview --compositor /home/zare/.local/share/background/bladerunner 5 0 0 1920 1080 &
+                ;;
+esac
+
+mpd &
+picom --no-frame-pacing &
+dwmblocks &
+dunst &
+exec dwm
 
